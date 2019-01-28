4b64f253e36331f9b8e2dbba868c9b3de971075d
Lervin <35772097+LervinU@users.noreply.github.com>
2019-01-28 13:03:47 -0500

    Update dbConnection.js

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -3,7 +3,7 @@ const mysql =  require('mysql');
 var connection =  mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'lervin29',
+    password: '*****',
     port: '3305',
     database: 'storageManager'
 });
